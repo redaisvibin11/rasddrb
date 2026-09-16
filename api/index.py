84780e016ff140d00ceb7739ab7eb303228b5a33
redaisvibin11 <redaisvibin211@gmail.com>
2026-09-16 08:14:15 +0100

    Move env file out of api directory

--- api/index.py
+++ api/index.py
@@ -40,3 +40,11 @@ def translate():
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/api/index", methods=["GET"])
+def handler():
+    return "OK"
